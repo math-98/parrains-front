33cc51cd388c0e4999d4168782ad537dd5bb72bd
Mathieu SERVIERE <mathieu@math98.fr>
2022-10-19 02:47:14 +0200

    App layout
     - Edit app general layout
     - Remove old components
     - Remove old views and routes

---
+++ src/layouts/FullpageLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-primary">
+    <div id="layoutAuthentication">
+      <div id="layoutAuthentication_content">
+        <main>
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-5">
+                <router-view></router-view>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+}
+</style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark mb-4">
+    <a class="navbar-brand ps-3" href="#">
+      <i class="fas fa-hands-helping"></i>
+      Parrainage
+    </a>
+
+    <ul class="navbar-nav ms-auto me-3 me-lg-4"></ul>
+  </nav>
+
+  <main>
+    <div class="container px-4">
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import { Modal } from "bootstrap";
+
+export default {
+  methods: {
+    logout() {
+      const modal = Modal.getInstance(document.getElementById("logoutModal"));
+      this.$store.dispatch("logout").then(() => {
+        modal.hide();
+      });
+    },
+  },
+  computed: {
+    ...mapState(["user"]),
+  },
+};
+</script>
